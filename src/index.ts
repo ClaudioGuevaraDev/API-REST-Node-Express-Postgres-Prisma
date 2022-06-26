97c7518c03a1d3cadb85863e1374388e161ea688
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-26 14:56:30 -0400

    Servidor de express iniciado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World")
+import app from "./app";
+import { APP_PORT } from "./config";
+
+function startServer() {
+  try {
+    app.listen(APP_PORT);
+    console.log("Server on port " + APP_PORT);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
